a3b8ab4a4c5e4cb13ab7d7675625430ed3ef3483
saitgurdag <sait.gurdag@gmail.com>
2025-02-25 13:58:52 +0300

    refactor: update cart component to use global state and improve item display

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-
-const cartItems = [
-  {
-    id: 1,
-    name: 'Product 1',
-    price: '$10',
-  },
-  {
-    id: 2,
-    name: 'Product 2',
-    price: '$15',
-  },
-  {
-    id: 3,
-    name: 'Product 3',
-    price: '$20',
-  },
-];
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { useStore } from "shell/store";
 
 const Cart = () => {
+  const { cart, removeFromCart } = useStore();
+
   return (
     <div>
-      <Typography mb={4} variant="h6">
-        This is your cart micro.
-      </Typography>
+      <Typography mb={4} variant="h6"></Typography>
       <Stack spacing={4}>
-        {cartItems.map(item => (
-          <Card
-            key={item.id}
-            sx={{ display: 'flex', alignItems: 'center', p: 2 }}
-          >
-            <CardContent sx={{ flex: 1 }}>
-              <Typography variant="h6">{item.name}</Typography>
-              <Typography color="text.secondary">{item.price}</Typography>
-            </CardContent>
-            <Button variant="contained" color="error">
-              Remove
-            </Button>
-          </Card>
-        ))}
+        {cart &&
+          cart.map((item) => (
+            <Card
+              key={item.id}
+              sx={{ display: "flex", alignItems: "center", p: 2 }}
+            >
+              <CardContent sx={{ flex: 1 }}>
+                <Typography variant="h6">{item.name}</Typography>
+
+                <Typography color="text.secondary">
+                  {item.quantity} x {item.price}
+                </Typography>
+                <Typography color="text.secondary">
+                  {item.price * item.quantity}
+                </Typography>
+              </CardContent>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => removeFromCart({ id: item.id })}
+              >
+                Remove
+              </Button>
+            </Card>
+          ))}
       </Stack>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
-const deps = require('./package.json').dependencies;
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
     compress: true,
     port: 3002,
     hot: true,
     historyApiFallback: true,
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
@@ -23,45 +23,46 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name: 'cart',
-      filename: 'remoteEntry.js',
+      name: "cart",
+      filename: "remoteEntry.js",
       exposes: {
-        './App': './src/App',
+        "./App": "./src/App",
       },
+      remotes: { shell: "shell@http://localhost:3000/remoteEntry.js" },
       shared: {
         ...deps,
         react: {
           singleton: true, // bu sayede tum uygulamalar tek bir react instance i uzerinden calisir
           requiredVersion: deps.react,
         },
-        'react-dom': {
+        "react-dom": {
           singleton: true,
-          requiredVersion: deps['react-dom'],
+          requiredVersion: deps["react-dom"],
         },
-        '@mui/material': {
+        "@mui/material": {
           singleton: true,
-          requiredVersion: deps['@mui/material'],
+          requiredVersion: deps["@mui/material"],
         },
-        '@mui/icons-material': {
+        "@mui/icons-material": {
           singleton: true,
-          requiredVersion: deps['@mui/icons-material'],
+          requiredVersion: deps["@mui/icons-material"],
         },
       },
     }),
